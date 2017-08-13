cc7b223feee3405340bdb15a33c48eb0c4625654
sduprey <stefan.duprey@yahoo.fr>
2017-08-13 04:57:50 -0700

    ICO failed/succeeded scenarios

--- deploy/crowdsaleScenarioTest.js
+++ deploy/crowdsaleScenarioTest.js
@@ -19,7 +19,7 @@ const recipient = web3.eth.accounts[2];
 web3.personal.unlockAccount(account,unlockingPassword, 1000);
 console.log(account);
 
-var crowdSaleContractAddress = "0xd0fab46ce75dfb3ba8650e00c2bd6095c3f8b9cf";
+var crowdSaleContractAddress = "0x41c8ca1c5ee16a65bfe9434df53b2c6c7d1dda7e";
 var napoleonXMultiSigWalletAddress  = "0xf1565df2b02ac6d756a1a594b180d34ee3438be8";
 
 var napoleonxcrowdsaleAbi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"MIN_TOKEN_AMOUNT_INVESTMENT","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"greenlistTotalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"investedAmountOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"bountyAllocated","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ETHER_MIN_CAP","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"presaleEndTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"endTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_startTime","type":"uint256"}],"name":"setStartTime","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"weiRaised","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"presaleEtherRaised","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"MAX_GREENLIST_CONTRIBUTION_DURATION","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"halt","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"min_cap","type":"uint256"},{"name":"max_cap","type":"uint256"}],"name":"setTestingCap","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"bountyLockup","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"startTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"getBackEtherTest","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"founderLockup","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newFounder","type":"address"}],"name":"changeFounder","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ETHER_MAX_CAP","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"FOUNDER_ALLOCATION","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"founderAllocated","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"bountyPercentage","type":"uint256"}],"name":"allocateBountyTokens","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"committer","type":"address"},{"name":"value","type":"uint256"}],"name":"registerCommitment","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"founderAllocationInPercent","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"halted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"allocateFounderTokens","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"committers","type":"address[]"},{"name":"values","type":"uint256[]"}],"name":"populateExistingGreenList","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"transferLockup","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"GREENLIST_DISCOUNT","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"presaleTokenSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unhalt","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_endTime","type":"uint256"}],"name":"setEndTime","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_presaleEndTime","type":"uint256"}],"name":"setPresaleEndTime","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"buyTokens","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"discountInPercent","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ONE_NPX_TOKEN_PRICE","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getICOStage","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"MAX_CONTRIBUTION_DURATION","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_committer","type":"address"}],"name":"commitmentOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"safeWithdrawal","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"_napoleonXMultiSigWallet","type":"address"},{"name":"setStartTime","type":"uint256"}],"payable":false,"type":"constructor"},{"payable":true,"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":false,"name":"eth","type":"uint256"},{"indexed":false,"name":"fbt","type":"uint256"}],"name":"Buy","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":false,"name":"eth","type":"uint256"},{"indexed":false,"name":"fbt","type":"uint256"}],"name":"PresaleBuy","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":false,"name":"to","type":"address"},{"indexed":false,"name":"eth","type":"uint256"}],"name":"Withdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"}],"name":"AllocateFounderTokens","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"}],"name":"AllocateBountyTokens","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"investor","type":"address"},{"indexed":false,"name":"weiAmount","type":"uint256"}],"name":"Refund","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}];
@@ -161,10 +161,10 @@ console.log(napoleonxcrowdsale.commitmentOf(web3.eth.accounts[4]));
 // estimating the fallback function gas
 // using the callback
 var greenlistData = napoleonxcrowdsale.populateExistingGreenList.getData(greenlist_committers,greenlist_committed_amounts);
-var estimate = web3.eth.estimateGas({data: greenlistData})
+var estimate = web3.eth.estimateGas({data: greenlistData});
 
 var buyTokensData=napoleonxcrowdsale.buyTokens.getData();
-var buyTokensDataEstimate = web3.eth.estimateGas({data: buyTokensData})
+var buyTokensDataEstimate = web3.eth.estimateGas({data: buyTokensData});
 
 // first presale entry
 // user has committed 2.5 ethers but does actually send 1 ether then 2.8 ethers
@@ -211,7 +211,19 @@ console.log(web3.eth.getBalance(crowdSaleContractAddress));
 // should 1*1.5*1.2 + 1.8-1.5
 console.log(napoleonxcrowdsale.balanceOf(web3.eth.accounts[2]));
 
+// total balance of crowdsale contract which acts as an escrow contract for the crowdsale duration
+// we are at 5.6 ethers : the c
+console.log(web3.eth.getBalance(crowdSaleContractAddress));
+console.log(napoleonxcrowdsale.ETHER_MIN_CAP());
+console.log(napoleonxcrowdsale.ETHER_MAX_CAP());
 
+// good scenario the crowd sale does reach its total : everything should be go to the multisig
+// good scenario the crowd sale does reach its total : everything should be go to the multisig
+// good scenario the crowd sale does reach its total : everything should be go to the multisig
+// good scenario the crowd sale does reach its total : everything should be go to the multisig
+// good scenario the crowd sale does reach its total : everything should be go to the multisig
+// good scenario the crowd sale does reach its total : everything should be go to the multisig
+// good scenario the crowd sale does reach its total : everything should be go to the multisig
 web3.personal.unlockAccount(account,unlockingPassword,1000);
 var artificial_endingTime_transaction = napoleonxcrowdsale.setEndTime.sendTransaction(
 startingTime,
@@ -233,6 +245,95 @@ console.log(napoleonxcrowdsale.startTime());
 console.log(napoleonxcrowdsale.endTime());
 console.log(napoleonxcrowdsale.presaleEndTime());
 
+///  every one can withdraw its investment
+var safeWithdrawalData=napoleonxcrowdsale.safeWithdrawal.getData();
+var safeWithdrawalDataEstimate = web3.eth.estimateGas({data: safeWithdrawalData});
+
+// first to withdraw is account 1 who should get back its 3.8 ethers
+console.log(web3.eth.getBalance(crowdSaleContractAddress));
+console.log(web3.eth.getBalance(web3.eth.accounts[1]));
+console.log(napoleonxcrowdsale.investedAmountOf(web3.eth.accounts[1]));
+web3.personal.unlockAccount(web3.eth.accounts[1],unlockingPassword,1000);
+var safeWithdrawal_transaction = napoleonxcrowdsale.safeWithdrawal.sendTransaction(
+ {
+        from: web3.eth.accounts[1],
+        gas: safeWithdrawalDataEstimate
+ });
+var MySafeWithDrawalReceipt = web3.eth.getTransactionReceiptMined(safeWithdrawal_transaction);
+console.log(web3.eth.getBalance(web3.eth.accounts[1]));
+console.log(napoleonxcrowdsale.investedAmountOf(web3.eth.accounts[1]));
+
+///  every one can withdraw its investment
+var safeWithdrawalData=napoleonxcrowdsale.safeWithdrawal.getData();
+var safeWithdrawalDataEstimate = web3.eth.estimateGas({data: safeWithdrawalData});
+
+// second to withdraw is account 1 who should get back its 1.8 ethers
+console.log(web3.eth.getBalance(crowdSaleContractAddress));
+console.log(web3.eth.getBalance(web3.eth.accounts[2]));
+console.log(napoleonxcrowdsale.investedAmountOf(web3.eth.accounts[2]));
+web3.personal.unlockAccount(web3.eth.accounts[2],unlockingPassword,1000);
+var safeWithdrawal_transaction = napoleonxcrowdsale.safeWithdrawal.sendTransaction(
+ {
+        from: web3.eth.accounts[2],
+        gas: safeWithdrawalDataEstimate
+ });
+var MySafeWithDrawalReceipt = web3.eth.getTransactionReceiptMined(safeWithdrawal_transaction);
+console.log(web3.eth.getBalance(web3.eth.accounts[2]));
+console.log(napoleonxcrowdsale.investedAmountOf(web3.eth.accounts[2]));
+
+// the crowdsale token escrow contract should be empty :
+console.log(web3.eth.getBalance(crowdSaleContractAddress));
+
+
+// bad scenario the crowd sale does not reach its total : everything should be reimbursed
+// bad scenario the crowd sale does not reach its total : everything should be reimbursed
+// bad scenario the crowd sale does not reach its total : everything should be reimbursed
+// bad scenario the crowd sale does not reach its total : everything should be reimbursed
+// bad scenario the crowd sale does not reach its total : everything should be reimbursed
+// bad scenario the crowd sale does not reach its total : everything should be reimbursed
+// bad scenario the crowd sale does not reach its total : everything should be reimbursed
+web3.personal.unlockAccount(account,unlockingPassword,1000);
+var capitalizationLimits_transaction = napoleonxcrowdsale.setTestingCap.sendTransaction(
+web3.toWei(6, "ether"),
+web3.toWei(7, "ether"),
+ {
+        from: account,
+        gas: 1000000
+ });
+var MyCapitalizationReceipt = web3.eth.getTransactionReceiptMined(capitalizationLimits_transaction);
+console.log(napoleonxcrowdsale.ETHER_MIN_CAP());
+console.log(napoleonxcrowdsale.ETHER_MAX_CAP());
+console.log(napoleonxcrowdsale.weiRaised());
+console.log(napoleonxcrowdsale.presaleTokenSupply());
+console.log(napoleonxcrowdsale.presaleEtherRaised());
+
+// artificially closing the auction
+var artificial_endingTime_transaction = napoleonxcrowdsale.setEndTime.sendTransaction(
+startingTime,
+ {
+        from: account,
+        gas: 1000000
+ });
+var MyArtificialEndingTimeReceipt = web3.eth.getTransactionReceiptMined(artificial_endingTime_transaction);
+var artificial_presaleEndingTime_transaction = napoleonxcrowdsale.setPresaleEndTime.sendTransaction(
+startingTime,
+ {
+        from: account,
+        gas: 1000000
+ });
+var MyArtificialPresaleEndingTimeReceipt = web3.eth.getTransactionReceiptMined(artificial_presaleEndingTime_transaction);
+
+console.log(napoleonxcrowdsale.getICOStage());
+console.log(napoleonxcrowdsale.startTime());
+console.log(napoleonxcrowdsale.endTime());
+console.log(napoleonxcrowdsale.presaleEndTime());
+
+// observing that the ICO has failed
+console.log(napoleonxcrowdsale.ETHER_MIN_CAP());
+console.log(napoleonxcrowdsale.ETHER_MAX_CAP());
+console.log(napoleonxcrowdsale.weiRaised());
+
+
 /// reimbursing every one
 var safeWithdrawalData=napoleonxcrowdsale.safeWithdrawal.getData();
 var safeWithdrawalDataEstimate = web3.eth.estimateGas({data: safeWithdrawalData});
@@ -242,6 +343,7 @@ console.log(web3.eth.getBalance(web3.eth.accounts[1]));
 console.log(web3.eth.getBalance(web3.eth.accounts[2]));
 console.log(web3.eth.getBalance(web3.eth.accounts[3]));
 
+web3.personal.unlockAccount(account,unlockingPassword,1000);
 var safeWithdrawal_transaction = napoleonxcrowdsale.safeWithdrawal.sendTransaction(
  {
         from: account,
